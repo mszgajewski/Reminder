b725410de312520e8937f523c615d02fc5d6ad0d
mszgajewski <mszgajewski@gmail.com>
2022-09-26 23:19:24 +0200

    InitialCommit

---
+++ app/src/main/java/com/mszgajewski/reminder/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.mszgajewski.reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+    private TextView loginQuestion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        loginEmail =findViewById(R.id.loginEmail);
+        loginPassword =findViewById(R.id.loginPassword);
+        loginButton =findViewById(R.id.loginButton);
+        loginQuestion =findViewById(R.id.loginPageQuestion);
+
+        loginQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
