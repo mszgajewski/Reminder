7e3e085cfb019106453f1efe8d9d593a212cf71f
mszgajewski <mszgajewski@gmail.com>
2023-05-23 22:09:19 +0200

    Commit

--- app/src/main/java/com/mszgajewski/reminder/MainActivity.java
+++ app/src/main/java/com/mszgajewski/reminder/MainActivity.java
@@ -1,16 +1,12 @@
 package com.mszgajewski.reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.os.Handler;
-
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-
 import com.mszgajewski.reminder.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
